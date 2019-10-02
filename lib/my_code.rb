ce39c8ad49d11c6f901da377a50dcd999b2e8890
Jonathan Norman <jnorman2123@gmail.com>
2019-10-02 11:37:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,17 @@ def reduce_to_total(source_array, starting_point = 0)
   starting_point 
 end 
 
-
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == true 
+      i += 1 
+    else 
+      return false 
+    end 
+  end 
+  return true 
+end 
 
 
 
