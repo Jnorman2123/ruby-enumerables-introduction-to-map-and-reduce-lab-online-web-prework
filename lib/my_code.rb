69c7b5c853d4a44ad3f486a5ca2e8a81559f4969
Jonathan Norman <jnorman2123@gmail.com>
2019-10-02 11:55:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,7 +59,17 @@ def reduce_to_all_true(source_array)
   return true 
 end 
 
-
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == false 
+      i += 1 
+    else
+      return true 
+    end 
+  end 
+  return false 
+end 
 
 
 
